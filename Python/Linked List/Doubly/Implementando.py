3719bf1117b7166b09eee7c1af8552bbd42771f8
Vitor <vitoremmanuel520@gmail.com>
2022-09-25 23:12:54 -0300

    Implemented more methods for doubly linked list

--- Python/Linked List/Doubly/Implementando.py
+++ Python/Linked List/Doubly/Implementando.py
@@ -48,6 +48,57 @@ class doublyLL:
             current_node.next = new_node
             new_node.previous = current_node
 
+    def before_insert(self,value,node):
+
+        new_node = Node(value)
+
+        if self.head is None:
+            print('The list is empty')
+            return
+
+        current_node = self.head
+
+        while current_node is not None:
+            if current_node.value == node:
+                break
+            current_node = current_node.next
+
+        if current_node is None:
+            print('Given Node is not present in the list')
+        else:
+            new_node.next = current_node
+            new_node.previous = current_node.previous
+            current_node.previous.next = new_node
+            current_node.previous = new_node
+
+    def after_insert(self,value,node):
+
+        new_node = Node(value)
+
+        if self.head is None:
+            print('The list is empty')
+            return
+
+        current_node = self.head
+
+        while current_node is not None:
+            if current_node.value == node:
+                break
+
+            current_node = current_node.next
+
+        if current_node is None:
+            print('Given Node is not present in the list')
+        else:
+            new_node.next = current_node.next
+            new_node.previous = current_node
+
+            if current_node.next is not None:
+                current_node.next.previous = new_node
+
+            current_node.next = new_node
+
+
     def print_ll(self):
         if self.head is None:
             print('List empty')
@@ -71,10 +122,45 @@ class doublyLL:
                 print(current_node.value, '-->', end=' ')
                 current_node = current_node.previous
 
+    def find(self,node):
+
+        if self.head is None:
+            print('List empty')
+
+        current_node = self.head
+
+        while self.head is not None:
+            if current_node.value == node:
+                break
+            current_node = current_node.next
+
+        if current_node is None:
+            print("Node not found")
+            return
+
+        self.print_node_status(current_node)
+
+        return current_node
+    def print_node_status(self,node):
+
+        try:
+            next = node.next.value
+        except AttributeError:
+            next = 'None'
+        try:
+            previous = node.previous.value
+        except AttributeError:
+            previous = 'None'
+
+        print(f'next:{previous}, prev:{next}')
 
 linked = doublyLL()
 
 linked.empty_insert(10)
 linked.begin_insert(50)
 linked.end_insert(20)
-linked.print_ll_reverser()
+linked.before_insert(100,10)
+linked.before_insert(200,100)
+linked.after_insert(60,200)
+a = linked.find(20)
+linked.print_ll()
