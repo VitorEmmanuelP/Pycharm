4c680e4307562cc8d1f453ce94ccd24705f280c1
Vitor <vitoremmanuel520@gmail.com>
2022-09-26 12:11:14 -0300

    Implemented more methods for doubly linked list, and fixed some methods

--- Python/Linked List/Doubly/Implementando.py
+++ Python/Linked List/Doubly/Implementando.py
@@ -1,7 +1,6 @@
 class Node:
 
-    def __init__(self,value):
-
+    def __init__(self, value):
         self.__value = value
         self.__next = None
         self.__previous = None
@@ -9,21 +8,27 @@ class Node:
     @property
     def value(self):
         return self.__value
+
     @value.setter
-    def value(self,data):
+    def value(self, data):
         self.__value = data
+
     @property
     def next(self):
         return self.__next
+
     @next.setter
-    def next(self,data):
+    def next(self, data):
         self.__next = data
+
     @property
     def previous(self):
         return self.__previous
+
     @previous.setter
-    def previous(self,data):
+    def previous(self, data):
         self.__previous = data
+
 
 class doublyLL:
 
@@ -31,11 +36,10 @@ class doublyLL:
 
         self.head = None
 
-    def __empty_insert(self,data):
+    def __empty_insert(self, data):
 
         new_node = Node(data)
         self.head = new_node
-
 
     def begin_insert(self, data):
 
@@ -48,7 +52,7 @@ class doublyLL:
             self.head.previous = new_node
             self.head = new_node
 
-    def end_insert(self,data):
+    def end_insert(self, data):
 
         if self.head is None:
             self.__empty_insert(data)
@@ -63,7 +67,7 @@ class doublyLL:
             current_node.next = new_node
             new_node.previous = current_node
 
-    def before_insert(self,value,node):
+    def before_insert(self, value, node):
 
         new_node = Node(value)
 
@@ -86,7 +90,7 @@ class doublyLL:
                 current_node.previous.next = new_node
                 current_node.previous = new_node
 
-    def after_insert(self,value,node):
+    def after_insert(self, value, node):
 
         new_node = Node(value)
 
@@ -112,7 +116,35 @@ class doublyLL:
 
                 current_node.next = new_node
 
-    def removing_middle(self,node):
+    def removing_head(self):
+
+        if self.head is None:
+            print('The list is empty')
+            return
+        if self.head.next is None:
+            self.head = None
+            print('List is empty after deleting the node!')
+        else:
+            self.head = self.head.next
+            self.head.previous = None
+
+    def removing_tail(self):
+
+        if self.head is None:
+            print('The list is empty')
+            return
+        if self.head.next is None:
+            self.head = None
+            print('List is empty after deleting the node!')
+
+        else:
+            current_node = self.head
+
+            while current_node.next is not None:
+                current_node = current_node.next
+
+            current_node.previous.next = None
+    def removing_middle(self, node):
 
         if self.head is None:
             print('The list is empty')
@@ -134,8 +166,7 @@ class doublyLL:
         else:
             current_node = self.head
             while current_node is not None:
-
-                print(current_node.value,'-->',end=' ')
+                print(current_node.value, '-->', end=' ')
                 current_node = current_node.next
 
     def print_ll_reverser(self):
@@ -150,7 +181,7 @@ class doublyLL:
                 print(current_node.value, '-->', end=' ')
                 current_node = current_node.previous
 
-    def find(self,node):
+    def find(self, node):
 
         if self.head is None:
             print('List empty')
@@ -166,11 +197,11 @@ class doublyLL:
             print("Node not found")
             return
 
-        self.print_node_status(current_node)
+        self.__print_node_status(current_node)
 
         return current_node
 
-    def print_node_status(self,node):
+    def __print_node_status(self, node):
 
         try:
             next = node.next.value
@@ -184,20 +215,23 @@ class doublyLL:
         print(f'next:{previous}, prev:{next}')
 
 
-
-
 linked = doublyLL()
-
 
 linked.begin_insert(10)
 linked.begin_insert(50)
 linked.end_insert(20)
-linked.before_insert(100,10)
-linked.before_insert(200,100)
-linked.after_insert(60,200)
-linked.after_insert(600,20)
+linked.before_insert(100, 10)
+linked.before_insert(200, 100)
+linked.after_insert(60, 200)
+linked.after_insert(600, 20)
 linked.print_ll()
 print('')
 linked.removing_middle(60)
-linked.find(60)
 linked.print_ll()
+print('')
+linked.removing_tail()
+linked.print_ll()
+print('')
+linked.removing_head()
+linked.print_ll()
+print('')
