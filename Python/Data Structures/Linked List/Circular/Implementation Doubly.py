6b1abf9642b3024cbc45240d03a9f14d448e06db
Vitor <vitoremmanuel520@gmail.com>
2022-09-27 16:18:10 -0300

    created more folders and fixed and implemented some methods

--- Python/Data Structures/Linked List/Circular/Implementation Doubly.py
+++ Python/Data Structures/Linked List/Circular/Implementation Doubly.py
@@ -39,18 +39,24 @@ class DoublyLL:
     def __empty_insert(self, data):
 
         new_node = Node(data)
+        new_node.next = new_node
+        new_node.previous = new_node
         self.head = new_node
 
     def begin_insert(self, data):
 
         if self.head is None:
             self.__empty_insert(data)
+            return
 
-        else:
-            new_node = Node(data)
-            new_node.next = self.head
-            self.head.previous = new_node
-            self.head = new_node
+        new_node = Node(data)
+
+        new_node.next = self.head
+        new_node.previous = self.head.previous
+        self.head.previous.next = new_node
+        self.head.previous = new_node
+        self.head = new_node
+
 
     def end_insert(self, data):
 
@@ -61,11 +67,17 @@ class DoublyLL:
             new_node = Node(data)
             current_node = self.head
 
-            while current_node.next is not None:
+            while current_node.next != self.head:
                 current_node = current_node.next
 
-            current_node.next = new_node
             new_node.previous = current_node
+            new_node.next = self.head
+            current_node.next = new_node
+            self.head.previous = new_node
+            # print(f'valor: {new_node.value}, next= {new_node.next.value}, prev: {new_node.previous.value}')
+
+            if current_node == self.head:
+                current_node.previous = new_node
 
     def before_insert(self, value, node):
 
@@ -75,23 +87,23 @@ class DoublyLL:
             print('The list is empty')
             return
 
-        else:
-            current_node = self.head
-            while current_node is not None:
-                if current_node.value == node:
-                    break
-                current_node = current_node.next
+        current_node = self.head
 
-            if current_node is None:
-                print('Given Node is not present in the list')
-            else:
-                new_node.next = current_node
-                new_node.previous = current_node.previous
-                if current_node.previous is not None:
-                    current_node.previous.next = new_node
-                else:
-                    self.head = new_node
-                current_node.previous = new_node
+        while current_node.next != self.head:
+            if current_node.value == node:
+                break
+            current_node = current_node.next
+
+        if current_node.value != node :
+            print('Given Node is not present in the list')
+            return
+
+        new_node.next = current_node
+        new_node.previous = current_node.previous
+        current_node.previous.next = new_node
+        current_node.previous = new_node
+        if current_node == self.head:
+            self.head = new_node
 
     def after_insert(self, value, node):
 
@@ -102,109 +114,113 @@ class DoublyLL:
         else:
             current_node = self.head
 
-            while current_node is not None:
+            while current_node.next != self.head:
                 if current_node.value == node:
                     break
 
                 current_node = current_node.next
 
-            if current_node is None:
+            if current_node.value != node:
                 print('Given Node is not present in the list')
-            else:
-                new_node.next = current_node.next
-                new_node.previous = current_node
+                return
 
-                if current_node.next is not None:
-                    current_node.next.previous = new_node
-
-                current_node.next = new_node
+            new_node.next = current_node.next
+            new_node.previous = current_node
+            current_node.next.previous = new_node
+            current_node.next = new_node
 
     def removing_head(self):
 
         if self.head is None:
             print('The list is empty')
             return
-        if self.head.next is None:
+
+        if self.head.next == self.head:
             self.head = None
             print('List is empty after deleting the node!')
+
         else:
+            self.head.next.previous = self.head.previous
+            self.head.previous.next = self.head.next
             self.head = self.head.next
-            self.head.previous = None
 
     def removing_tail(self):
 
         if self.head is None:
             print('The list is empty')
             return
-        if self.head.next is None:
+        if self.head.next == self.head:
             self.head = None
             print('List is empty after deleting the node!')
-
+            return
         else:
-            current_node = self.head
+            self.head.previous = self.head.previous.previous
+            self.head.previous.next = self.head
 
-            while current_node.next is not None:
-                current_node = current_node.next
-
-            current_node.previous.next = None
 
     def removing_middle(self, node):
+
         if self.head is None:
             print('The list is empty')
             return
 
         current_node = self.head
 
-        while current_node is not None:
+        while current_node.next != self.head:
             if current_node.value == node:
                 break
             current_node = current_node.next
 
-        if current_node.previous is None:
-            current_node.next.previous = None
-            self.head = current_node.next
-        elif current_node.next is None:
-            current_node.previous.next = None
+        if current_node == self.head:
+            self.removing_head()
 
+        elif current_node.next == self.head:
+            self.removing_tail()
         else:
             current_node.next.previous = current_node.previous
             current_node.previous.next = current_node.next
 
     def print_ll(self):
-        if self.head is None:
-            print('List empty')
 
+        if self.head is None:
+            return 'Lista Vazia'
         else:
             current_node = self.head
-            while current_node is not None:
+
+            while current_node.next != self.head:
                 print(current_node.value, '-->', end=' ')
                 current_node = current_node.next
+
+            print(current_node.value, '-->')
 
     def print_ll_reverser(self):
         if self.head is None:
             print('List empty')
         else:
             current_node = self.head
-            while current_node.next is not None:
-                current_node = current_node.next
-
-            while current_node is not None:
+            print(current_node.previous.value)
+            while current_node.previous != self.head:
                 print(current_node.value, '-->', end=' ')
                 current_node = current_node.previous
+                if current_node.previous == self.head:
+                    break
+            print(current_node.value, '-->', end=' ')
+
 
     def find(self, node):
 
         if self.head is None:
             print('List empty')
-
+            return
         current_node = self.head
 
-        while current_node is not None:
+        while current_node.next != self.head:
+
             if current_node.value == node:
                 break
             current_node = current_node.next
 
-        if current_node is None:
+        if current_node.value != node:
             print("Node not found")
             return
 
@@ -213,34 +229,40 @@ class DoublyLL:
         return current_node
 
     def __print_node_status(self, node):
-        try:
-            next = node.next.value
-        except AttributeError:
-            next = 'None'
-        try:
-            previous = node.previous.value
-        except AttributeError:
-            previous = 'None'
 
-        print(f'next:{previous}, prev:{next}')
+        next = node.next.value
+        previous = node.previous.value
+
+        print(f'next:{next}, prev:{previous}')
 
 
 linked = DoublyLL()
-
 linked.begin_insert(10)
-linked.end_insert(60)
-linked.before_insert(50,10)
-linked.print_ll()
-print('')
+linked.end_insert(20)
+linked.end_insert(30)
+linked.end_insert(40)
+# linked.end_insert(50)
+# linked.end_insert(6000)
+#
 
-linked.removing_middle(60)
+linked.before_insert(60,10)
+
+linked.after_insert(365,40)
+
+
+
+linked.removing_head()
+linked.removing_tail()
 linked.print_ll()
-print('')
-linked.removing_middle(50)
+
+linked.removing_middle(40)
+
 linked.print_ll()
-print('')
-linked.removing_middle(10)
-linked.print_ll()
+linked.find(20)
+
+
+
+# linked.print_ll_reverser()
 
 # print('')
 # linked.removing_middle(60)
