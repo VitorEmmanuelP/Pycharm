3362e1214852f7319f41fce1ecb13f147b0f73ae
Vitor <vitoremmanuel520@gmail.com>
2022-09-30 21:16:11 -0300

    add comments

--- Python/Algoritimo/Binary Search/Implementation.py
+++ Python/Algoritimo/Binary Search/Implementation.py
@@ -1,4 +1,10 @@
-def binary_search(list_of_numbers, key):
+# Begin with the mid_element of the whole array as a search key.
+# If the value of the search key is equal to the item then return an index of the search key.
+# Or if the value of the search key is less than the item in the middle of the interval, narrow the interval to the lower half.
+# Otherwise, narrow it to the upper half.
+# Repeatedly check from the second point until the value is found or the interval is empty.
+
+def binary_search(list_of_numbers, key_value):
 
     low = 0
     high = len(list_of_numbers) - 1
@@ -26,3 +32,4 @@ lista.sort()
 key = int(input('Enter the key: '))
 
 binary_search(lista,key)
+
